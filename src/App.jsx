03e0ecd9c73c1ac0eea3fd8bbebd51bcd7a7a6f1
Dev-Pritom <pritomsaha480@gmail.com>
2026-08-12 11:39:26 +0600

    add files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1>Deployment App</h1>
+    <p>This is a simple deployment app.</p>
+   </div>
+  )
+}
+
+export default App
